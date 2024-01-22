e3e3eb713d289973d595053f4e0c03b6d2c5c83a
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-22 16:55:46 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/regiscostapedro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.regiscostapedro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.regiscostapedro.workshopmongo.domain.User;
+import com.regiscostapedro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
