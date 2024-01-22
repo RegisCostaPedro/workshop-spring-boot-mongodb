595bb625a3938f9153890ea7ea778f6e25ec4e16
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-22 15:23:06 -0300

    Fist commit - Project created

---
+++ src/test/java/com/regiscostapedro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regiscostapedro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
