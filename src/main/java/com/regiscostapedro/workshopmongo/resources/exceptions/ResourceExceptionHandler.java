74daadf248c65e3940219a2d943ea04bee284fc0
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-23 14:20:00 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/regiscostapedro/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.regiscostapedro.workshopmongo.resources.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.regiscostapedro.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(),status.value(),"Não encontrado",e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	// Metodo que retorna um erro 404 caso o findById não funcione
+}
