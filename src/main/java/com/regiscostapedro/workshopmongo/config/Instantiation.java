e586af5e0459180483c19b91e9502fd92cfec03b
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-23 16:35:57 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/regiscostapedro/workshopmongo/config/Instantiation.java
+++ src/main/java/com/regiscostapedro/workshopmongo/config/Instantiation.java
@@ -41,7 +41,7 @@ public class Instantiation implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null,sdf.parse("21/01/2018"),"Partiu viagem!","Vou viajar para São Paulo abraços!",new AuthorDTO(maria));
-		Post post2 = new Post(null,sdf.parse("23/01/2018"),"Bom dia!","/acordei feliz hoje LMAO!",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/01/2018"),"Bom dia!","acordei feliz hoje LMAO!",new AuthorDTO(maria));
 		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
--- src/main/java/com/regiscostapedro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/regiscostapedro/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.regiscostapedro.workshopmongo.domain.Post;
 import com.regiscostapedro.workshopmongo.domain.User;
 import com.regiscostapedro.workshopmongo.dto.UserDTO;
 import com.regiscostapedro.workshopmongo.services.UserService;
@@ -70,5 +71,14 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 }
 
