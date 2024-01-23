63cdc92ece2aefcd5c9250f72d2fcc469c1072d0
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-23 16:30:50 -0300

    Referenciando os posts do usuário

--- src/main/java/com/regiscostapedro/workshopmongo/config/Instantiation.java
+++ src/main/java/com/regiscostapedro/workshopmongo/config/Instantiation.java
@@ -36,15 +36,18 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
 		//criar primeiro os users para assim depois ele ter a cópia para o DTO post
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
 		Post post1 = new Post(null,sdf.parse("21/01/2018"),"Partiu viagem!","Vou viajar para São Paulo abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/01/2018"),"Bom dia!","/acordei feliz hoje LMAO!",new AuthorDTO(maria));
 		
-		;
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
