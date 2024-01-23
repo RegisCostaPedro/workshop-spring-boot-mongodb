072ae30cc44a82b1d00d123016be5473a1d9b38c
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-23 17:23:12 -0300

    Consulta simples com query methods

--- src/main/java/com/regiscostapedro/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/regiscostapedro/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.regiscostapedro.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,13 @@ import com.regiscostapedro.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+//QUERY METHODS
+//"Buscar posts contendo um dado string no título"
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
+
+
 	
--- src/main/java/com/regiscostapedro/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/regiscostapedro/workshopmongo/repository/UserRepository.java
@@ -8,4 +8,5 @@ import com.regiscostapedro.workshopmongo.domain.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
+	
 }
